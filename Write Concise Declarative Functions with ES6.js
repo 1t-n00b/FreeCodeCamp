fb4e335aa1362528c94b2d3e11b628481af4cbfc
1t-n00b <mummintrol@gmail.com>
2021-12-26 20:11:28 +0200

    solution task

---
+++ Write Concise Declarative Functions with ES6.js
@@ -0,0 +1,12 @@
+// Only change code below this line
+const bicycle = {
+    gear: 2,
+    setGear(gear){
+        this.gear = 48;
+    }
+
+
+};
+// Only change code above this line
+bicycle.setGear(3);
+console.log(bicycle.gear);
